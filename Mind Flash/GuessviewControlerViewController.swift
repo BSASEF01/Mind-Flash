b067571e5907ff5757bddf6fa7d95c4fc98a6454
BSASEF01 <bersabehsolomon16@gmail.com>
2018-03-20 19:33:40 -0400

    add questions

--- Mind Flash/GuessviewControlerViewController.swift
+++ Mind Flash/GuessviewControlerViewController.swift
@@ -21,22 +21,19 @@ class GuessviewControlerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
+       
     }
     
 
     func loadQuestions() {
-       let question1 = Question(question: "what type of movie is The Hunger Game ?", answer: [], correctanswer: 1)
-        
-        
-        
-        
-        
+        let question1 = Question(question: "what type of movie is The Hunger Game ?", answer: ["Adventure","Comedy", "Family","none"], correctanswer: 1)
+        let question2 = Question(question: "20% of 2 equal to ", answer: [ "20", "4", "0.4", "0.04" ], correctanswer: 3)
+        let question3 = Question(question: "what is the differnt between pant cell and animal cell?", answer: ["cell wall","Nucleus","Mitochndria","Plasma Membrane"], correctanswer: 1)
         
         questionArray.append(question1)
         
